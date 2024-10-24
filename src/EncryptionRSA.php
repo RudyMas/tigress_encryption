00ab25a3e203e80d3ff9aae38c2256d80d653a01
RudyMasGoNext <rudy.mas@go-next.be>
2024-10-24 14:24:42 +0200

    Update RSA

--- src/EncryptionRSA.php
+++ src/EncryptionRSA.php
@@ -10,7 +10,7 @@ use phpseclib3\Crypt\RSA;
  * @author       Rudy Mas <rudy.mas@rudymas.be>
  * @copyright    2024, Rudy Mas (http://rudymas.be/)
  * @license      https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version      1.0.0
+ * @version      1.0.1
  * @lastmodified 2024-10-24
  * @package      Tigress
  */
@@ -21,16 +21,16 @@ class EncryptionRSA extends Encryption
      *
      * @param int $bits
      * @param string $type
-     * @param string $password
+     * @param string|false $password
      * @return string
      */
-    public function createPrivateKey(int $bits, string $type = 'PKCS1', string $password = ''): string
+    public function createPrivateKey(int $bits, string $type = 'PKCS1', mixed $password = false): string
     {
         $key = RSA::createKey($bits);
-        if (empty($password)) {
-            return $key->toString($type);
+        if (!empty($password)) {
+            return $key->withPassword($password)->toString($type);
         }
-        return $key->toString($type, ['password' => $password]);
+        return $key->toString($type);
     }
 
     /**
@@ -38,10 +38,10 @@ class EncryptionRSA extends Encryption
      *
      * @param string $privateKey
      * @param string $type
-     * @param string $password
+     * @param string|false $password
      * @return string
      */
-    public function createPublicKey(string $privateKey, string $type = 'PKCS1', string $password = ''): string
+    public function createPublicKey(string $privateKey, string $type = 'PKCS1', mixed $password = false): string
     {
         $key = RSA::load($privateKey, $password);
         return $key->getPublicKey()->toString($type);
